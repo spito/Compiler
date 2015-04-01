d4cd7cbc0f4e8eee345b54242e7fb14ff3e248b8
spito <jiri.weiser@gmail.com>
2015-04-01 21:30:56 +0200

    common: Add file into Position.

--- Compiler/common/Position.h
+++ Compiler/common/Position.h
@@ -3,6 +3,9 @@
 #include <utility>
 #include <cstddef>
 #include <iostream>
+#include <string>
+
+#include "../context/File.h"
 
 namespace compiler {
 namespace common {
@@ -11,16 +14,18 @@ class Position {
     size_t _rawPosition;
     size_t _line;
     size_t _column;
+    const std::string *_file;
 public:
 
     static Position nowhere() {
-        return Position( 0, 0, 0 );
+        return Position( 0, 0, 0, nullptr );
     }
 
     Position() :
         _rawPosition( 0 ),
         _line( 1 ),
-        _column( 1 )
+        _column( 1 ),
+        _file( &context::file() )
     {}
 
     Position( const Position & ) = default;
@@ -36,10 +41,11 @@ public:
     }
 
 private:
-    Position( size_t p, size_t l, size_t r ) :
+    Position( size_t p, size_t l, size_t r, const std::string *f ) :
         _rawPosition( p ),
         _line( l ),
-        _column( r )
+        _column( r ),
+        _file( f )
     {}
 
 public:
@@ -65,11 +71,17 @@ public:
     size_t position() const {
         return _rawPosition;
     }
+    const std::string &file() const {
+        return _file ?
+            *_file :
+            context::emptyString();
+    }
 
     void reset() {
         _rawPosition = 0;
         _line = 1;
         _column = 1;
+        _file = &context::file();
     }
 
     void swap( Position &p ) {
@@ -83,7 +95,8 @@ public:
         return
             _rawPosition == p._rawPosition &&
             _line == p._line &&
-            _column == p._column;
+            _column == p._column &&
+            _file == p._file;
     }
     bool operator!=( const Position &p ) const {
         return !( *this == p );
