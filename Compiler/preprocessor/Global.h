1fb77ff51a20274f3d0ab439603c77c1b43d75f2
spito <jiri.weiser@gmail.com>
2015-04-03 11:55:13 +0200

    preprocessor: Add missing class used by Worker.

---
+++ Compiler/preprocessor/Global.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../common/SymbolTable.h"
+#include "../common/TokenStore.h"
+#include "Symbol.h"
+#include "Substituer.h"
+
+namespace compiler {
+namespace preprocessor {
+
+struct Global {
+    common::SymbolTable< Symbol > symbols;
+    common::TokenStore store;
+    std::unordered_set< const std::string * > seenFiles;
+};
+
+
+} // namespace preprocessor
+} // namespace compiler
