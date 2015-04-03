4cfe56cf8f73b37261c1970f4d448c23a291c59d
spito <jiri.weiser@gmail.com>
2015-04-03 11:45:12 +0200

    common: Fix formating.

--- Compiler/common/Token.h
+++ Compiler/common/Token.h
@@ -3,265 +3,203 @@
 #include "Position.h"
 #include "Utils.h"
 #include "Unicode.h"
+#include "Operator.h"
 
 #include <string>
 #include <iostream>
 
 namespace compiler {
-    namespace common {
+namespace common {
 
-        struct Token : Comparable {
+struct Token : Comparable {
 
-            enum class Type {
-                Operator,
-                Integer,
-                Real,
-                Word,
-                Keyword,
-                String,
-                Char,
-                StringInclude,
-                Space,
-                NewLine,
-                FileBegin,
-                FileEnd,
-                Eof,
-            };
-
-            enum class Operator {
-
-                BraceOpen, // {
-                BraceClose,
-
-                // rank 1, left to right
-                Increment,
-                Decrement,
-                BracketOpen, // (
-                BracketClose,
-                BracketIndexOpen, // [
-                BracketIndexClose,
-
-                // rank 2, right to left
-                UnaryPlus,
-                UnaryMinus,
-                LogicalNot,
-                BitwiseNote,
-                //TypeCast, // (int)
-                Dereference,
-                AddressOf,
-                //Sizeof, // sizeof(int)
-
-                // rank 3, left to right
-                Multiplication,
-                Division,
-                Modulo,
-
-                // rank 4, left to right
-                Addition,
-                Subtraction,
-
-                // rank 5, left to right
-                BitwiseLeftShift,
-                BitwiseRightShift,
-
-                // rank 6, left to right
-                LessThan,
-                LessThenOrEqual,
-                GreaterThan,
-                GreaterThanOrEqual,
-
-                // rank 7, left to right
-                EqualTo,
-                NotEqualTo,
-
-                // rank 8, left to right
-                BitwiseAnd,
-
-                // rank 9, left to right
-                BitwiseXor,
-
-                // rank 10, left to right
-                BitwiseOr,
-
-                // rank 11, left to right
-                LogicalAnd,
-
-                // rank 12, left to right
-                LogicalOr,
-
-                // rank 13, right to left
-                //TernaryOperator
-
-                // rank 14, right to left
-                Assignment,
-                AssignmentSum,
-                AssignmentDifference,
-                AssignmentProduct,
-                AssignmentQuotient,
-                AssignmentRemainder,
-                AssignmentLeftShift,
-                AssignmentRightShift,
-                AssignmentBitwiseAnd,
-                AssignmentBitwiseXor,
-                AssignmentBitwiseOr,
-
-                // rank 15, left to right
-                Comma,
-            };
-
-        private:
-            std::string _token;
-            Type _type;
-            Position _position;
-
-            union _N {
-                long long integer;
-                long double real;
-
-                _N( long long i ) : integer( i ) {}
-                _N( long double r ) : real( r ) {}
-                _N() = default;
-            } _number;
-
-        public:
-
-            Token( Type t = Type::Eof, Position p = Position() ) :
-                Token( std::move( t ), std::string(), std::move( p ) )
-            {}
-            
-            Token( Type t, std::string token, Position p = Position() ) :
-                _token( std::move( token ) ),
-                _type( t ),
-                _position( std::move( p ) )
-            {}
-
-            Token( const Token &o ) :
-                _token( o._token ),
-                _type( o._type ),
-                _position( o._position )
-            {
-                if ( _type == Type::Integer )
-                    _number.integer = o._number.integer;
-                else if ( _type == Type::Real )
-                    _number.real = o._number.real;
-            }
-
-            Token( Token &&o ) :
-                _token( std::move( o._token ) ),
-                _type( o._type ),
-                _position( std::move( o._position ) )
-            {
-                if ( _type == Type::Integer )
-                    _number.integer = o._number.integer;
-                else if ( _type == Type::Real )
-                    _number.real = o._number.real;
-            }
-
-            void swap( Token &o ) {
-                using std::swap;
-                swap( _token, o._token );
-                swap( _type, o._type );
-                swap( _position, o._position );
-                swap( _number, o._number );
-            }
+    enum class Type {
+        Operator,
+        Integer,
+        Real,
+        Word,
+        Keyword,
+        String,
+        Char,
+        StringInclude,
+        Space,
+        NewLine,
+        FileBegin,
+        FileEnd,
+        Eof,
+    };
 
 
-            Token &operator=( Token t ) {
-                swap( t );
-                return *this;
-            }
+private:
+    std::string _token;
+    Type _type;
+    Position _position;
 
-            size_t size() const {
-                return _token.size();
-            }
+    union _N {
+        long long integer;
+        long double real;
+        Operator op;
 
-            Type type() const {
-                return _type;
-            }
+        _N( long long i ) : integer( i ) {}
+        _N( long double r ) : real( r ) {}
+        _N( Operator o ) : op( o ) {}
+        _N() = default;
+    } _detail;
 
-            Position &position() {
-                return _position;
-            }
-            const Position &position() const {
-                return _position;
-            }
+public:
 
+    Token( Type t = Type::Eof, Position p = Position() ) :
+        Token( std::move( t ), std::string(), std::move( p ) )
+    {}
 
-            std::string &value() {
-                return _token;
-            }
+    Token( Type t, std::string token, Position p = Position() ) :
+        _token( std::move( token ) ),
+        _type( t ),
+        _position( std::move( p ) )
+    {}
 
-            const std::string &value() const {
-                return _token;
-            }
-
-            long long &integer() {
-                return _number.integer;
-            }
-            long long integer() const {
-                return _number.integer;
-            }
-
-            long double &real() {
-                return _number.real;
-            }
-            long double real() const {
-                return _number.real;
-            }
-
-            char &operator[]( size_t index ) {
-                return _token[ index ];
-            }
-            char operator[]( size_t index ) const {
-                return _token[ index ];
-            }
-
-            auto begin()
-                -> decltype( _token.begin() )
-            {
-                return _token.begin();
-            }
-            auto end()
-                -> decltype( _token.end() )
-            {
-                return _token.end();
-            }
-            auto begin() const
-                -> decltype( _token.begin() )
-            {
-                return _token.begin();
-            }
-            auto end() const
-                -> decltype( _token.end() )
-            {
-                return _token.end();
-            }
-
-
-            bool operator==( const Token &o ) const {
-                return
-                    _type == o._type &&
-                    _token == o._token;
-            }
-
-            bool identical( const Token &o ) const {
-                return
-                    *this == o &&
-                    _position == o._position;
-            }
-
-        };
-        inline std::ostream &operator<<( std::ostream &out, Token token ) {
-            if ( token.type() == Token::Type::String )
-                return out << '"' << Unicode::toAscii( token.value() ) << '"';
-            return out << token.value();
-        }
+    Token( const Token &o ) :
+        _token( o._token ),
+        _type( o._type ),
+        _position( o._position )
+    {
+        if ( _type == Type::Integer )
+            integer() = o.integer();
+        else if ( _type == Type::Real )
+            real() = o.real();
+        else if ( _type == Type::Operator )
+            op() = o.op();
     }
+
+    Token( Token &&o ) :
+        _token( std::move( o._token ) ),
+        _type( o._type ),
+        _position( std::move( o._position ) )
+    {
+        if ( _type == Type::Integer )
+            integer() = o.integer();
+        else if ( _type == Type::Real )
+            real() = o.real();
+        else if ( _type == Type::Operator )
+            op() = o.op();
+    }
+
+    void swap( Token &o ) {
+        using std::swap;
+        swap( _token, o._token );
+        swap( _type, o._type );
+        swap( _position, o._position );
+        swap( _detail, o._detail );
+    }
+
+
+    Token &operator=( Token t ) {
+        swap( t );
+        return *this;
+    }
+
+    size_t size() const {
+        return _token.size();
+    }
+
+    Type type() const {
+        return _type;
+    }
+
+    Position &position() {
+        return _position;
+    }
+    const Position &position() const {
+        return _position;
+    }
+
+
+    std::string &value() {
+        return _token;
+    }
+
+    const std::string &value() const {
+        return _token;
+    }
+
+    long long &integer() {
+        return _detail.integer;
+    }
+    long long integer() const {
+        return _detail.integer;
+    }
+
+    long double &real() {
+        return _detail.real;
+    }
+    long double real() const {
+        return _detail.real;
+    }
+
+    Operator &op() {
+        return _detail.op;
+    }
+    Operator op() const {
+        return _detail.op;
+    }
+    bool isOperator( Operator o ) const {
+        return type() == Type::Operator && op() == o;
+    }
+
+    char &operator[]( size_t index ) {
+        return _token[ index ];
+    }
+    char operator[]( size_t index ) const {
+        return _token[ index ];
+    }
+
+    auto begin()
+        -> decltype( _token.begin() )
+    {
+        return _token.begin();
+    }
+    auto end()
+        -> decltype( _token.end() )
+    {
+        return _token.end();
+    }
+    auto begin() const
+        -> decltype( _token.begin() )
+    {
+        return _token.begin();
+    }
+    auto end() const
+        -> decltype( _token.end() )
+    {
+        return _token.end();
+    }
+
+
+    bool operator==( const Token &o ) const {
+        return
+            _type == o._type &&
+            _token == o._token;
+    }
+
+    bool identical( const Token &o ) const {
+        return
+            *this == o &&
+            _position == o._position;
+    }
+
+};
+inline std::ostream &operator<<( std::ostream &out, Token token ) {
+    if ( token.type() == Token::Type::String )
+        return out << '"' << Unicode::toAscii( token.value() ) << '"';
+    return out << token.value();
 }
+
+} // namespace common
+} // namespace compiler
 
 namespace std {
-    template<>
-    inline void swap( ::compiler::common::Token &lhs, ::compiler::common::Token &rhs ) {
-        lhs.swap( rhs );
-    }
+template<>
+inline void swap( ::compiler::common::Token &lhs, ::compiler::common::Token &rhs ) {
+    lhs.swap( rhs );
 }
+} // namespace std
--- Compiler/preprocessor/Symbol.h
+++ Compiler/preprocessor/Symbol.h
@@ -6,109 +6,109 @@
 #include <vector>
 
 namespace compiler {
-    namespace preprocessor {
+namespace preprocessor {
 
-        struct Symbol : common::Symbol {
+struct Symbol : common::Symbol {
 
-            using Base = common::Symbol;
+    using Base = common::Symbol;
 
-            enum class Kind {
-                Nothing,
-                Integer,
-                String,
-                Function
-            };
+    enum class Kind {
+        Nothing,
+        Integer,
+        String,
+        Function
+    };
 
-            Symbol( std::string name ) :
-                Base( std::move( name ) ),
-                _kind( Kind::Nothing )
-            {}
+    Symbol( std::string name ) :
+        Base( std::move( name ) ),
+        _kind( Kind::Nothing )
+    {}
 
-            Symbol( std::string name, common::Token token ) :
-                Base( std::move( name ) ),
-                _kind( token.type() == common::Token::Type::Integer ? Kind::Integer : Kind::String ),
-                _integer( token.integer() )
-            {
-                _value.push_back( std::move( token ) );
-            }
-            Symbol( std::string name, std::vector< common::Token > value ) :
-                Base( std::move( name ) ),
-                _kind( Kind::String ),
-                _value( std::move( value ) )
-            {}
-            Symbol( std::string name, std::vector< common::Token > parametres, std::vector< common::Token > value ) :
-                Base( std::move( name ) ),
-                _kind( Kind::Function ),
-                _value( std::move( value ) ),
-                _parametres( std::move( parametres ) )
-            {}
+    Symbol( std::string name, common::Token token ) :
+        Base( std::move( name ) ),
+        _kind( token.type() == common::Token::Type::Integer ? Kind::Integer : Kind::String ),
+        _integer( token.integer() )
+    {
+        _value.push_back( std::move( token ) );
+    }
+    Symbol( std::string name, std::vector< common::Token > value ) :
+        Base( std::move( name ) ),
+        _kind( Kind::String ),
+        _value( std::move( value ) )
+    {}
+    Symbol( std::string name, std::vector< common::Token > parametres, std::vector< common::Token > value ) :
+        Base( std::move( name ) ),
+        _kind( Kind::Function ),
+        _value( std::move( value ) ),
+        _parametres( std::move( parametres ) )
+    {}
 
-            Symbol( const Symbol & ) = default;
-            Symbol( Symbol &&other ) :
-                Base( std::move( other ) ),
-                _kind( other._kind ),
-                _integer( other._integer ),
-                _value( std::move( other._value ) ),
-                _parametres( std::move( other._parametres ) )
-            {}
-       
-            Symbol &operator=( const Symbol & ) = default;
-            Symbol &operator=( Symbol &&other ) {
-                swap( other );
-                return *this;
-            }
+    Symbol( const Symbol & ) = default;
+    Symbol( Symbol &&other ) :
+        Base( std::move( other ) ),
+        _kind( other._kind ),
+        _integer( other._integer ),
+        _value( std::move( other._value ) ),
+        _parametres( std::move( other._parametres ) )
+    {}
 
-            Kind kind() const {
-                return _kind;
-            }
+    Symbol &operator=( const Symbol & ) = default;
+    Symbol &operator=( Symbol &&other ) {
+        swap( other );
+        return *this;
+    }
 
-            long long integer() const {
-                if ( _kind != Kind::Integer )
-                    throw exception::InternalError( "Invalid usage of symbol (not an integer)" );
-                return _integer;
-            }
-            const std::vector< common::Token > &value() const {
-                return _value;
-            }
-            const std::vector< common::Token > &parametres() const {
-                if ( _kind != Kind::Function )
-                    throw exception::InternalError( "Invalid usage of symbol (not a function)" );
-                return _parametres;
-            }
+    Kind kind() const {
+        return _kind;
+    }
 
-            void swap( Symbol &other ) {
-                using std::swap;
-                swap( _kind, other._kind );
-                swap( _integer, other._integer );
-                swap( _value, other._value );
-                swap( _parametres, other._parametres );
-            }
+    long long integer() const {
+        if ( _kind != Kind::Integer )
+            throw exception::InternalError( "Invalid usage of symbol (not an integer)" );
+        return _integer;
+    }
+    const std::vector< common::Token > &value() const {
+        return _value;
+    }
+    const std::vector< common::Token > &parametres() const {
+        if ( _kind != Kind::Function )
+            throw exception::InternalError( "Invalid usage of symbol (not a function)" );
+        return _parametres;
+    }
 
-            bool identical( const Symbol &other ) const {
-                if ( kind() != other.kind() || name() != other.name() )
-                    return false;
+    void swap( Symbol &other ) {
+        using std::swap;
+        swap( _kind, other._kind );
+        swap( _integer, other._integer );
+        swap( _value, other._value );
+        swap( _parametres, other._parametres );
+    }
 
-                switch ( kind() ) {
-                case Symbol::Kind::Nothing:
-                    return true;
-                case Symbol::Kind::Integer:
-                    return integer() == other.integer();
-                case Symbol::Kind::String:
-                    return value() == other.value();
-                case Symbol::Kind::Function:
-                    return value() == other.value() &&
-                        parametres() == other.parametres();
-                default:
-                    throw exception::InternalError( "Invalid kind of symbol" );
-                }
-            }
-            
-        private:
-            Kind _kind;
-            long long _integer;
-            std::vector< common::Token > _value;
-            std::vector< common::Token > _parametres;
-        };
+    bool identical( const Symbol &other ) const {
+        if ( kind() != other.kind() || name() != other.name() )
+            return false;
 
-    } // namespace preprocessor
+        switch ( kind() ) {
+        case Symbol::Kind::Nothing:
+            return true;
+        case Symbol::Kind::Integer:
+            return integer() == other.integer();
+        case Symbol::Kind::String:
+            return value() == other.value();
+        case Symbol::Kind::Function:
+            return value() == other.value() &&
+                parametres() == other.parametres();
+        default:
+            throw exception::InternalError( "Invalid kind of symbol" );
+        }
+    }
+
+private:
+    Kind _kind;
+    long long _integer;
+    std::vector< common::Token > _value;
+    std::vector< common::Token > _parametres;
+};
+
+} // namespace preprocessor
 } // namespace compiler
