3f527dc2ba5c5a3f3d1d8ecd34c87253353add20
spito <jiri.weiser@gmail.com>
2015-05-02 16:31:49 +0200

    ast: Add MemoryHolder class.

---
+++ Compiler/ast/MemoryHolder.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "Type.h"
+
+#include <string>
+#include <map>
+
+namespace compiler {
+namespace ast {
+
+struct MemoryHolder {
+
+    struct Variable {
+
+        Variable( int o, type::Type *t ) :
+            _offset( o ),
+            _type( *t )
+        {}
+
+        int offset() const {
+            return _offset;
+        }
+        int size() const {
+            return _type.size();
+        }
+        const type::Type &type() const {
+            return _type;
+        }
+
+    private:
+        int _offset;
+        type::Type &_type;
+    };
+
+    MemoryHolder() :
+        _memoryLength( 0 )
+    {}
+
+    void add( std::string name, type::Type *type ) {
+        if ( _variables.count( name ) )
+            throw std::runtime_error( "duplicit symbol" );
+        _variables.emplace( std::move( name ), Variable( _memoryLength, type ) );
+        _memoryLength += type->size();
+    }
+
+    int memoryLength() const {
+        return _memoryLength;
+    }
+
+    template< typename Yield >
+    void forVariables( Yield yield ) const {
+        for ( const auto &i : _variables )
+            yield( i.first, i.second );
+    }
+
+    size_t size() const {
+        return _variables.size();
+    }
+
+private:
+    std::map< std::string, Variable > _variables;
+    int _memoryLength;
+};
+
+
+} // namespace ast
+} // namespace compiler
