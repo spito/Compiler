b0e67e1e259f7c0be96fa67435fbed9c0cd634a9
spito <jiri.weiser@gmail.com>
2015-05-07 12:47:06 +0200

    ast: Prepare for introduction of initializers.

--- Compiler/ast/Core.h
+++ Compiler/ast/Core.h
@@ -3,24 +3,6 @@
 namespace compiler {
 namespace ast {
 
-//template< typename Traversal > struct Expression;
-//template< typename Traversal > struct UnaryOperator;
-//template< typename Traversal > struct BinaryOperator;
-//template< typename Traversal > struct TernaryOperator;
-//template< typename Traversal > struct Call;
-//template< typename Traversal > struct Variable;
-//template< typename Traversal > struct Constant;
-//template< typename Traversal > struct Block;
-//template< typename Traversal > struct If;
-//template< typename Traversal > struct DoWhile;
-//template< typename Traversal > struct While;
-//template< typename Traversal > struct For;
-//template< typename Traversal > struct StatementSwitch;
-////struct StatementEcho;
-//template< typename Traversal > struct Break;
-//template< typename Traversal > struct Continue;
-//template< typename Traversal > struct Return;
-
 struct Expression;
 struct UnaryOperator;
 struct BinaryOperator;
@@ -28,12 +10,14 @@ struct TernaryOperator;
 struct Call;
 struct Variable;
 struct Constant;
+struct ArrayInitializer;
+struct StringPlaceholder;
 struct Block;
 struct If;
 struct DoWhile;
 struct While;
 struct For;
-struct StatementSwitch;
+//struct StatementSwitch;
 //struct StatementEcho;
 struct Break;
 struct Continue;
@@ -46,6 +30,8 @@ enum class Kind {
     Call,
     Variable,
     Constant,
+    ArrayInitializer,
+    StringPlaceholder,
     Block,
     If,
     DoWhile,
